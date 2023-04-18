f5eefca12ac2200649599881bba971bee0c6c8e1
lunathanael <68858103+lunathanael@users.noreply.github.com>
2023-04-17 23:44:16 -0400

    Revert "Attempted new move scoring for early cutoffs"
    This reverts commit 111fdb206a3926398a6f16d425c46bb366637816.

--- BadChess/search.cpp
+++ BadChess/search.cpp
@@ -3,9 +3,6 @@
 #include "stdio.h"
 #include "defs.h"
 #include <iostream>
-#include <algorithm>
-
-
 
 
 // Check if the time is up or interrupt from GUI
@@ -19,13 +16,12 @@ static void CheckUp(S_SEARCHINFO *info) {
 }
 
 
-
 // Find the best score in the remaning moves, move ordering
 static void PickNextMove(int moveNum, S_MOVELIST* list) {
 
 	S_MOVE temp;
 	int index = 0;
-	int bestScore = -2147483645;
+	int bestScore = -INF_BOUND;
 	int bestNum = moveNum;
 
 	// Find best score
@@ -41,58 +37,6 @@ static void PickNextMove(int moveNum, S_MOVELIST* list) {
 	list->moves[bestNum] = temp;
 }
 
-
-
-static inline void score_moves(S_BOARD* pos, S_SEARCHINFO* info, S_MOVELIST* move_list, int PvMove) {
-	//Loop through all the move in the movelist
-	for (int i = 0; i < move_list->count; ++i) {
-		int move = move_list->moves[i].move;
-		//If the move is from the TT (aka it's our hashmove) give it the highest score
-		if (move == PvMove) {
-			move_list->moves[i].score = INT32_MAX - 100;
-			continue;
-		}
-		else if (move & MFLAGPROM) {
-			move_list->moves[i].score = 2000000000 + PROMOTED(move);
-			continue;
-		}
-		//if the move is an enpassant or a promotion give it a score that a good capture of type pawn-pawn would have
-		else if (move & MFLAGEP) {
-			move_list->moves[i].score = 105 + 900000000; // Good capture score
-			continue;
-		}
-		//if the move is a capture sum the mvv-lva score to a variable that depends on whether the capture has a positive SEE or not 
-		else if (move & MFLAGCAP) {
-			move_list->moves[i].score =
-				MvvLvaScores[CAPTURED(move)][pos->pieces[FROMSQ(move)]] + 900000000;// *SEE(pos, move, -107);
-			continue;
-		}
-		//First  killer move always comes after the TT move,the promotions and the good captures and before anything else
-		else if (pos->searchKillers[0][pos->ply] == move) {
-			move_list->moves[i].score = 800000000; // Killer move 1
-			continue;
-		}
-		//Second killer move always comes after the first one
-		else if (pos->searchKillers[1][pos->ply] == move) {
-			move_list->moves[i].score = 700000000; // Kilelr move 2
-			continue;
-		}
-		////After the killer moves try the Counter moves
-		//else if (move == info->CounterMoves[FROMSQ(ss->move)][TOSQ(ss->move)])
-		//{
-		//	move_list->moves[i].score = 600000000;
-		//	continue;
-		//}
-		//if the move isn't in any of the previous categories score it according to the history heuristic
-		else {
-			move_list->moves[i].score = pos->searchHistory[pos->pieces[FROMSQ(move)]][TOSQ(move)];
-			//move_list->moves[i].score = GetHHScore(pos, sd, move) + 2 * GetCHScore(pos, sd, ss, move);
-			continue;
-		}
-	}
-
-	return;
-}
 
 // Search for a position repetition
 static int IsRepetition(const S_BOARD* pos) {
@@ -149,7 +93,7 @@ static void ClearForSearch(S_BOARD* pos, S_SEARCHINFO* info) {
 
 
 // Prevent horizon effect with Quiescence search
-static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info, int depth) {
+static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info) {
 	
 	// Valid board
 	ASSERT(CheckBoard(pos));
@@ -164,25 +108,14 @@ static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info, int
 
 	// Repetition
 	if (IsRepetition(pos) || pos->fiftyMove >= 100) {
-		return 1 - (info->nodes & 2); // Prevent 3-fold blindness
+		return 0;
 	}
-
 	// Max Depth
 	if (pos->ply > MAXDEPTH - 1) {
-		int InCheck = SqAttacked(pos->KingSq[pos->side], pos->side ^ 1, pos);
-		return InCheck ? 0 : EvalPosition(pos);
+		return EvalPosition(pos);
 	}
-
-	int PvMove = NOMOVE;
 
 	int Score = EvalPosition(pos);
-
-	// Probe hash entry
-	if (ProbeHashEntry(pos, &PvMove, &Score, alpha, beta, depth) == TRUE) {
-		++pos->HashTable->cut;
-		return Score;
-	}
-
 	// Standing pat
 	if (Score >= beta) {
 		return beta;
@@ -194,8 +127,6 @@ static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info, int
 	// Generate captures
 	S_MOVELIST list[1];
 	GenerateAllCaptures(pos, list);
-
-	score_moves(pos, info, list, PvMove);
 
 	// Variable definitions
 	int MoveNum = 0;
@@ -213,7 +144,7 @@ static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info, int
 		}
 
 		++Legal;
-		Score = -Quiescence(-beta, -alpha, pos, info, depth - 1); // Recursively call function
+		Score = -Quiescence(-beta, -alpha, pos, info); // Recursively call function
 		TakeMove(pos);
 
 		// Check status
@@ -238,9 +169,6 @@ static int Quiescence(int alpha, int beta, S_BOARD* pos, S_SEARCHINFO* info, int
 }
 
 
-
-
-
 // NegaMax Alpha Beta pruning search
 static int AlphaBeta(int alpha, int beta, int depth, S_BOARD* pos, S_SEARCHINFO* info, int DoNull) {
 
@@ -251,7 +179,7 @@ static int AlphaBeta(int alpha, int beta, int depth, S_BOARD* pos, S_SEARCHINFO*
 	ASSERT(depth >= 0);
 	// At terminal node
 	if (depth <= 0) {
-		return Quiescence(alpha, beta, pos, info, depth - 1);
+		return Quiescence(alpha, beta, pos, info);
 		// return EvalPosition(pos);
 	}
 
@@ -305,8 +233,6 @@ static int AlphaBeta(int alpha, int beta, int depth, S_BOARD* pos, S_SEARCHINFO*
 	S_MOVELIST list[1];
 	GenerateAllMoves(pos, list);
 
-	score_moves(pos, info, list, PvMove);
-
 	// Variable definitions
 	int MoveNum = 0;
 	int Legal = 0;
@@ -316,18 +242,16 @@ static int AlphaBeta(int alpha, int beta, int depth, S_BOARD* pos, S_SEARCHINFO*
 	int BestScore = -INF_BOUND;
 	Score = -INF_BOUND;
 
-	//// Prioritize Principal variation move, CHANGE*
-	//if (PvMove != NOMOVE) {
-	//	for (MoveNum = 0; MoveNum < list->count; ++MoveNum) {
-	//		if (list->moves[MoveNum].move == PvMove) {
-	//			list->moves[MoveNum].score = 2000000; 
-	//			//printf("Pv move found \n");
-	//			break;
-	//		}
-	//	}
-	//}
-
-
+	// Prioritize Principal variation move
+	if (PvMove != NOMOVE) {
+		for (MoveNum = 0; MoveNum < list->count; ++MoveNum) {
+			if (list->moves[MoveNum].move == PvMove) {
+				list->moves[MoveNum].score = 2000000;
+				//printf("Pv move found \n");
+				break;
+			}
+		}
+	}
 
 	for (int MoveNum = 0; MoveNum < list->count; ++MoveNum) {
 
@@ -402,51 +326,6 @@ static int AlphaBeta(int alpha, int beta, int depth, S_BOARD* pos, S_SEARCHINFO*
 }
 
 
-
-
-int AspirationWindowSearch(int prev_eval, int depth, S_BOARD* pos, S_SEARCHINFO* info) {
-	int score = 0;
-
-	//Search_stack stack[MAXDEPTH], * ss = stack;
-
-	//We set an expected window for the score at the next search depth, this window is not 100% accurate so we might need to try a bigger window and re-search the position
-	int delta = 12;
-	// define initial alpha beta bounds
-	int alpha = -INF_BOUND;
-	int beta = INF_BOUND;
-
-	// only set up the windows is the search depth is bigger or equal than Aspiration_Depth to avoid using windows when the search isn't accurate enough
-	if (depth >= 3) {
-		alpha = std::max(-INF_BOUND, prev_eval - delta);
-		beta = std::min(prev_eval + delta, INF_BOUND);
-	}
-
-	//Stay at current depth if we fail high/low because of the aspiration windows
-	while (true) {
-
-		score = AlphaBeta(alpha, beta, depth, pos, info, TRUE);
-
-		// stop calculating and return best move so far
-		if (info->stopped) break;
-
-		// we fell outside the window, so try again with a bigger window, if we still fail after we just search with a full window
-		if ((score <= alpha)) {
-			beta = (alpha + beta) / 2;
-			alpha = std::max(-INF_BOUND, score - delta);
-		}
-
-		// we fell outside the window, so try again with a bigger window, if we still fail after we just search with a full window
-		else if ((score >= beta)) {
-			beta = std::min(score + delta, INF_BOUND);
-		}
-		else break;
-		delta *= 1.44;
-	}
-	return score;
-}
-
-
-
 // Iterative deepening from depth = 1 to MAXDEPTH
 void SearchPosition(S_BOARD* pos, S_SEARCHINFO* info) {
 
@@ -468,8 +347,7 @@ void SearchPosition(S_BOARD* pos, S_SEARCHINFO* info) {
 		for (currentDepth = 1; currentDepth <= info->depth; ++currentDepth) {
 
 			// Start alpha beta search
-			//bestScore = AlphaBeta(-INF_BOUND, INF_BOUND, currentDepth, pos, info, TRUE);
-			bestScore = AspirationWindowSearch(bestScore, currentDepth, pos, info);
+			bestScore = AlphaBeta(-INF_BOUND, INF_BOUND, currentDepth, pos, info, TRUE);
 
 			// Check status
 			if (info->stopped == TRUE) {
